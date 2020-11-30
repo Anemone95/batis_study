45dc8b67adc58ed898987302e7d626eaf0cb3b49
Anemone95 <x565178035@126.com>
2020-11-30 15:47:45 +0800

    Update createTable.sql

--- mybatisdemo2/src/main/resources/createTable.sql
+++ mybatisdemo2/src/main/resources/createTable.sql
@@ -1,8 +1,8 @@
 use test;
-create table user(
+create table users(
     id INT(10) AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(10) NOT NULL,
     password VARCHAR(32) NOT NULL,
-    user_sex INTEGER,
+    user_sex VARCHAR(32),
     nick_name VARCHAR(10) NOT NULL
 )
